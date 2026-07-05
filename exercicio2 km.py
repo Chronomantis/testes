c1d588d88ee1c5f4edb6cb1ef8b58f729b0de9de
Leonardo Nadalim <lnadalim@gmail.com>
2026-07-05 17:48:29 -0300

    Add files via upload

---
+++ exercicio2 km.py
@@ -0,0 +1,6 @@
+km = int(input('quantos km foram percorridos?'))
+dias = int(input('quantos dias foram percorridos?'))
+
+preco = 60 * dias + 0.15 * km
+print(f'km = {km}. Dias : {dias}. ')
+print(f'Valor a ser pago: {preco}')
